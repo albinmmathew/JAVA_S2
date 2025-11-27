08166e034f5f10c1c8568f9a057693922f2ab10c
albinmmathew <albinmmathew@gmail.com>
2025-11-27 22:28:12 +0530

    P3 Update

--- PROGRAM_02.java
+++ PROGRAM_02.java
@@ -1,47 +1,50 @@
 /*PROGRAM 2 PERFORMING MATHEMATICAL OPERATIONS USING SWITCH CASE
 @ALBIN MAMMEN MATHEW
 Roll No: 08 
-17/11/2025
+Date: 17/11/2025
 */
 
 import java.io.*;
+
 class PROGRAM_02 {
-    public static void main(String args[]) {
-        if (args.length < 3) {
-            System.out.println("Usage: java CLA1 <num1> <num2> <option>");
-            return;
-        }
-        int n1 = Integer.parseInt(args[0]);
-        int n2 = Integer.parseInt(args[1]);
-        int n3 = Integer.parseInt(args[2]);
-        int opp;
-        switch (n3) {
-            case 1:
-                opp = n1 + n2;
-                System.out.println("Result: " + opp);
-                break;
-
-            case 2:
-                opp = n1 - n2;
-                System.out.println("Result: " + opp);
-                break;
-
-            case 3:
-                opp = n1 * n2;
-                System.out.println("Result: " + opp);
-                break;
-
-            case 4:
-                if (n2 == 0) {
-                    System.out.println("Error: Division by zero!");
-                } else {
-                    opp = n1 / n2;
-                    System.out.println("Result: " + opp);
-                }
-                break;
-            default:
-                System.out.println("INVALID OPTION");
-                break;
-        }
-    }
+	public static void main(String args[]) {
+		//Checking number of arguments
+		if (args.length < 3) {
+			System.out.println("Usage: java PROGRAM_02 <num1> <num2> <option>");
+			return;
+		}
+		int n1 = Integer.parseInt(args[0]);
+		int n2 = Integer.parseInt(args[1]);
+		int n3 = Integer.parseInt(args[2]);
+		int opp;
+		switch (n3) {
+			case 1:	//Addition
+				opp = n1 + n2;
+				System.out.println("Result: " + opp);
+				break;
+				
+			case 2:	//Substraction
+				opp = n1 - n2;
+				System.out.println("Result: " + opp);
+				break;
+			
+			case 3:	//Multiplication
+				opp = n1 * n2;
+				System.out.println("Result: " + opp);
+				break;
+				
+			case 4:	//Division
+				if (n2 == 0) {
+					System.out.println("Error: Division by zero!");
+				} else {
+					opp = n1 / n2;
+					System.out.println("Result: " + opp);
+				}
+				break;
+				
+			default:
+				System.out.println("INVALID OPTION");
+				break;
+		}
+	}
 }
---
+++ PROGRAM_03.java
@@ -0,0 +1,22 @@
+/*PROGRAM 3 PREDICTION OF SECRET NUMBER
+@ALBIN MAMMEN MATHEW
+Roll No: 08 
+Date: 20/11/2025
+*/
+
+import java.io.*;
+import java.util.*;
+
+class PROGRAM_03 {
+	public static void main(String[] args) {
+		int key = 24, count = 0, guess;
+		Scanner s = new Scanner(System.in);
+		do{
+			System.out.println("Predict a Number between 20 and 30 :");
+			guess = s.nextInt();
+			count++;
+		} while(guess != key);
+		System.out.println("Congrats!!");
+		System.out.println("You have predicted the secret number in " + count + " attempts.");
+	}
+}
