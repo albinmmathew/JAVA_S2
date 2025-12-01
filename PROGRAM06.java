bb2a30ee405d56ff7111ffc48ab9758854291ea3
albinmmathew <albinmmathew@gmail.com>
2025-12-02 00:30:39 +0530

    Added  Menu

--- PROGRAM06.java
+++ PROGRAM06.java
@@ -7,16 +7,16 @@ Date: 27/11/2025
 import java.io.*;
 import java.util.*;
 class MOpp{
+	Scanner s = new Scanner(System.in);
 	//Function to read and return a matrix 
 	public int[][] read(){
-		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the number of rows and columns : ");
 		int r = s.nextInt();
 		int c = s.nextInt();
 		int mat[][] = new int[r][c];
+		System.out.println("Enter the elements :");
 		for (int i = 0; i < r; i++){
 			for (int j = 0; j < c; j++){
-				System.out.println("Enter the element at [" + i + "][" + j + "]:");
 				mat[i][j] = s.nextInt();
 			}
 		}
@@ -47,14 +47,14 @@ class MOpp{
 			System.out.println("The given matrices have different Dimension.");
 		}
 	}
-	//Function to multiply two matrices if dimensions allign
+	//Function to multiply two matrices if dimensions align
 	public void multiply(int a[][], int b[][]){
 		if(a[0].length == b.length){
 			int mult[][] = new int[a.length][b[0].length];
 			for (int i = 0; i < a.length; i++){
 				for (int j = 0; j < b[0].length; j++){
 					mult[i][j] =0;
-					for(int k = 0; k < b.length; k++){
+					for(int k = 0; k < a[0].length; k++){
 						mult[i][j] += a[i][k] * b[k][j];
 					}
 				}
@@ -66,32 +66,54 @@ class MOpp{
 			System.out.println("The given matrices can't be multiplied due to difference in the dimensions of the matrix");
 		}
 	}
+	//Function for Menu
+	public void menu(){
+		int a[][], b[][];
+		while (true) {
+			System.out.println("1 - Addition");
+			System.out.println("2 - Multiplication");
+			System.out.println("3 - Exit");
+			System.out.println("Enter Your choice: ");
+			int choice = s.nextInt();
+			switch(choice) {
+				case 1:
+					System.out.println("ADDITION");
+					System.out.println("Enter the first matrix:");
+					a = read();
+					System.out.println("Enter the Second matrix:");
+					b = read();
+					System.out.println("First matrix:");
+					display(a);
+					System.out.println("Second matrix:");
+					display(b);
+					sum(a,b);
+					break;
+				case 2:
+					System.out.println("MULTIPLICATION:");
+					System.out.println("Enter the first matrix:");
+					a = read();
+					System.out.println("Enter the Second matrix:");
+					b = read();
+					System.out.println("First matrix:");
+					display(a);
+					System.out.println("Second matrix:");
+					display(b);
+					multiply(a,b);
+					break;
+				case 3:
+					System.exit(0);
+				default:
+					System.out.println("Invalid choice.");
+					break;
+			}
+		}
+	}
+
 }
 
 class PROGRAM06 {
 	public static void main(String args[]){	
 		MOpp obj  = new MOpp();
-		int a[][], b[][];
-		System.out.println("ADDITION");
-		System.out.println("Enter the first matrix:");
-		a = obj.read();
-		System.out.println("Enter the Second matrix:");
-		b = obj.read();
-		System.out.println("First matrix:");
-		obj.display(a);
-		System.out.println("Second matrix:");
-		obj.display(b);
-		obj.sum(a,b);
-		System.out.println();
-		System.out.println("MULTIPLICATION:");
-		System.out.println("Enter the first matrix:");
-		a = obj.read();
-		System.out.println("Enter the Second matrix:");
-		b = obj.read();
-		System.out.println("First matrix:");
-		obj.display(a);
-		System.out.println("Second matrix:");
-		obj.display(b);
-		obj.multiply(a,b);
+		obj.menu();
 	}
 }
